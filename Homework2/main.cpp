d1f41b69e0d1e514ba519c687f4e211e8773e78d
Maxim <maximborodin1@gmail.com>
2018-12-03 00:01:31 +0300

    Added some fixes

--- Homework2/main.cpp
+++ Homework2/main.cpp
@@ -3,46 +3,57 @@
 #include "fibonacciNumber.h"
 
 
-template <typename Root, typename Branch> struct Vertex;
+template<typename Root, typename Branch>
+struct Vertex;
 
-template <typename Root, typename Branch>
-struct Vertex : public Branch {};
+template<typename Root, typename Branch>
+struct Vertex : public Branch {
+};
 
 
-class EmptyClass {};
+class EmptyClass {
+};
 
 // линейная иерархия
-template <typename Arr, template<typename Root, typename Branch> typename Vertex>
+template<typename Arr, template<typename Root, typename Branch> typename Vertex>
 struct Bamboo;
 
-template <typename Head, typename ...Tail, template<typename Root, typename Branch> typename Vertex>
-struct Bamboo<TypeList<Head, Tail...>, Vertex> : public Vertex<Head, Bamboo<TypeList<Tail...>, Vertex> > {};
+template<typename Head, typename ...Tail, template<typename Root, typename Branch> typename Vertex>
+struct Bamboo<TypeList<Head, Tail...>, Vertex> : public Vertex<Head, Bamboo<TypeList<Tail...>, Vertex> > {
+};
 
-template <template <typename Root, typename Branch> typename Vertex>
-struct Bamboo<EmptyTypeList, Vertex> : public Vertex<EmptyClass, EmptyClass> {};
+template<template<typename Root, typename Branch> typename Vertex>
+struct Bamboo<EmptyTypeList, Vertex> : public Vertex<EmptyClass, EmptyClass> {
+};
 
 
 //Основная иерархия
-template <typename Arr, typename StoredArr, int remainingSize, int branchNumber,
-template <typename Root, typename Branch> typename Vertex>
+template<typename Arr, typename StoredArr, int remainingSize, int branchNumber,
+        template<typename Root, typename Branch> typename Vertex>
 struct Hierarchy;
 
 
-template <typename Head, typename ...Tail, typename ...StoredArr, typename ...Tail, int remainingSize,
-        int branchNumber, template <typename Root, typename Branch> typename Vertex>
+template<typename Head, typename ...Tail, typename ...StoredArr, typename ...Tail, int remainingSize,
+        int branchNumber, template<typename Root, typename Branch> typename Vertex>
 struct Hierarchy<TypeList<Head, Tail...>, TypeList<StoredArr...>, remainingSize, branchNumber, Vertex> :
-        public Hierarchy<TypeList<Tail...>, TypeList<Head, StoredArr...>, remainingSize - 1, branchNumber, Vertex>
-{};
+        public Hierarchy<TypeList<Tail...>, TypeList<Head, StoredArr...>, remainingSize - 1, branchNumber, Vertex>{};
 
-template <typename Head, typename ... Tail, typename ...StoredArr, int branchNumber,
-template <typename Root, typename Branch> class Vertex>
-        struct Hierarchy<TypeList<Head, Tail...>, typename <StoredArr...>, 0, branchNumber, Vertex> :
-                public Bamboo<TypeList<StoredArr...>, Vertex>,
-public Hierarchy<TypeList<Head, Tail...> EmptyTypeList, branchNumber + 1, Fibonacci<branchNumber + 1>::number + 1, Vertex>
-{};
-
-
-int main()
+template<typename Head, typename ... Tail, typename ...StoredArr, int branchNumber,
+        template<typename Root, typename Branch> class Vertex>
+struct Hierarchy<TypeList<Head, Tail...>, typename <StoredArr...>, 0, branchNumber, Vertex> :
+public Bamboo<TypeList<StoredArr...>, Vertex>,
+public Hierarchy<TypeList<Head, Tail...> EmptyTypeList,
+        branchNumber + 1, Fibonacci<branchNumber + 1>::number + 1, Vertex>
 {
+};
+
+template<typename ...StoredArr, int remainingSize, int branchNumber,
+        template<typename Root, typename Branch> class Unit>
+struct Hierarchy<EmptyTypeList, TypeList<StoredArr...>, remainingSize, branchNumber, Vertex> :
+        public Bamboo<TypeList<StoredArr...>, Vertex> {
+};
+
+
+int main() {
     // std::cout << Fibonacci<5>::number;
 }
